bed0433d2e91b8f2a7ad29b386bb4e4ca8fe306d
dianaloz <dlbogota2104@gmail.com>
2023-08-08 15:08:29 -0400

    Added changes using session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -13,20 +13,34 @@ responses = []
 
 @app.route("/")
 def homepage():
+    # extract the title and the instruction from surveys.py file
+    # using satisfaction_survey, which calls Survey class and also Question class
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template("startsurvey.html", title=title, instructions=instructions)
 
 
+@app.route("/handle-responses", methods=['GET', 'POST'])
+def handle_sessions_responses():
+    # Set the session responses to an empty list.
+    session["responses"] = []
+    # Redirect to the start of the survey
+    return redirect("/questions/0")
+
+
 @app.route("/questions/<numb>", methods=['GET', 'POST'])
 def currentquestion(numb):
     eachnumb = int(numb)
-    if (len(responses) == len(satisfaction_survey.questions)):
+    count = len(session["responses"])
+    # check if all the answers have been answered to thank the customer
+    if (count == len(satisfaction_survey.questions)):
         return render_template("thankyou.html")
-    elif (eachnumb > len(responses)):
-        count = len(responses)
+    # check if the number of the question is lower than the number of answers provided by customer
+    # if number is lower, redirect them to the the appropiate question based on the answers provided
+    elif (eachnumb > count):
         flash("You missed some questions of our valuable survey. Redirecting you.")
         return redirect("/questions/" + str(count))
+    # create question and choices based on the index number
     else:
         question = satisfaction_survey.questions[eachnumb].question
         choices = satisfaction_survey.questions[eachnumb].choices
@@ -35,7 +49,13 @@ def currentquestion(numb):
 
 @app.route("/answer", methods=["GET", "POST"])
 def handle_answers():
+    # get the answer from each question
     answer = request.form["choice"]
+    # append the answer to session["responses"]
+    responses = session["responses"]
     responses.append(answer)
-    count = len(responses)
+    session["responses"] = responses
+    # Check the number of answers in the responses list
+    # Based on that provide the next number for the next question
+    count = len(session["responses"])
     return redirect("/questions/" + str(count))
